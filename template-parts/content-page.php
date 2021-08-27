728ecfdb97e3d15beec441d77e2a7fe68f008285
Georgi <georgy.keranov@gmail.com>
2021-08-27 16:49:14 +0300

    Remove functions that aren't declared in the theme

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,8 +14,6 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php gk_post_thumbnail(); ?>
-
 	<div class="entry-content">
 		<?php
 		the_content();
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -12,24 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			gk_posted_on();
-			gk_posted_by();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
-
-	<?php gk_post_thumbnail(); ?>
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php gk_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
