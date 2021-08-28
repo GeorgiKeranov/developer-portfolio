f2f8779b17246b7ddca4c8232ced7fc1fe6500bf
Georgi <georgy.keranov@gmail.com>
2021-08-28 19:39:39 +0300

    Add mobile styles for footer and navigation

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 					wp_nav_menu( array(
 						'theme_location' => 'header-menu',
 						'container' => 'nav',
-						'container_class' => 'nav'
+						'container_class' => 'nav nav--footer'
 					) );
 				} ?>
 			</div><!-- /.footer__menu -->
