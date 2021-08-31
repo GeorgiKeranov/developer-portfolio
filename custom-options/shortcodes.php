dce9ebc290b2f3128086694177059758de8763d3
Georgi <georgy.keranov@gmail.com>
2021-09-01 02:54:32 +0300

    Add shortcode and styles for button

--- custom-options/shortcodes.php
+++ custom-options/shortcodes.php
@@ -9,3 +9,23 @@ add_shortcode( 'year', 'gk_shortcode_year' );
 function gk_shortcode_year() {
 	return date( 'Y' );
 }
+
+/**
+ * Button Shortcode
+ */
+add_shortcode( 'button', 'gk_shortcode_button' );
+function gk_shortcode_button( $atts, $content ) {
+	$atts = shortcode_atts( array(
+			'link' => '#',
+		), $atts, 'button'
+	);
+
+	ob_start();
+	?>
+	<a class="btn" href="<?php echo esc_url( $atts['link'] ) ?>"><?php echo $content ?></a>
+	<?php
+	$html = ob_get_clean();
+
+	return $html;
+}
+
