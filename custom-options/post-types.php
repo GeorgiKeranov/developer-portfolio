20b2b3f78beed898948fb0a47d049d5722045597
Georgi <georgy.keranov@gmail.com>
2021-09-03 20:05:58 +0300

    Add new post type "Project"

--- custom-options/post-types.php
+++ custom-options/post-types.php
@@ -1,30 +1,30 @@
 <?php
 
-// register_post_type( 'gk_custom_taxonomy', array(
-// 	'labels' => array(
-// 		'name'               => __( 'Custom Taxonomies', 'gk' ),
-// 		'singular_name'      => __( 'Custom Taxonomy', 'gk' ),
-// 		'add_new'            => __( 'Add New', 'gk' ),
-// 		'add_new_item'       => __( 'Add New Custom Taxonomy', 'gk' ),
-// 		'view_item'          => __( 'View Custom Taxonomy', 'gk' ),
-// 		'edit_item'          => __( 'Edit Custom Taxonomy', 'gk' ),
-// 		'new_item'           => __( 'New Custom Taxonomy', 'gk' ),
-// 		'view_item'          => __( 'View Custom Taxonomy', 'gk' ),
-// 		'search_items'       => __( 'Search Custom Taxonomies', 'gk' ),
-// 		'not_found'          => __( 'Not Found Custom Taxonomies', 'gk' ),
-// 		'not_found_in_trash' => __( 'Not Found Custom Taxonomies In Trash', 'gk' ),
-// 	),
-// 	'public'                => true,
-// 	'exclude_from_search'   => false,
-// 	'show_ui'               => true,
-// 	'capability_type'       => 'post',
-// 	'hierarchical'          => false,
-// 	'_edit_link'            => 'post.php?post=%d',
-// 	'rewrite'               => array(
-// 		'slug' => 'custom-taxonomy',
-// 		'with_front' => false,
-// 	),
-// 	'query_var'             => true,
-// 	'menu_icon'             => 'dashicons-welcome-write-blog',
-// 	'supports'              => array( 'title', 'thumbnail', 'editor' ),
-// ) );
+register_post_type( 'gk_project', array(
+	'labels' => array(
+		'name'               => __( 'Projects', 'gk' ),
+		'singular_name'      => __( 'Project', 'gk' ),
+		'add_new'            => __( 'Add New', 'gk' ),
+		'add_new_item'       => __( 'Add New Project', 'gk' ),
+		'view_item'          => __( 'View Project', 'gk' ),
+		'edit_item'          => __( 'Edit Project', 'gk' ),
+		'new_item'           => __( 'New Project', 'gk' ),
+		'view_item'          => __( 'View Project', 'gk' ),
+		'search_items'       => __( 'Search Projects', 'gk' ),
+		'not_found'          => __( 'Not Found Projects', 'gk' ),
+		'not_found_in_trash' => __( 'Not Found Projects In Trash', 'gk' ),
+	),
+	'public'                => true,
+	'exclude_from_search'   => false,
+	'show_ui'               => true,
+	'capability_type'       => 'post',
+	'hierarchical'          => false,
+	'_edit_link'            => 'post.php?post=%d',
+	'rewrite'               => array(
+		'slug' => 'project',
+		'with_front' => false,
+	),
+	'query_var'             => true,
+	'menu_icon'             => 'dashicons-welcome-write-blog',
+	'supports'              => array( 'title', 'thumbnail', 'editor', 'excerpt' ),
+) );
