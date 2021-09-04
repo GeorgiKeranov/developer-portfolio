90675a1840a6a4a3c97e2476ec468a6fe8d8efbd
Georgi <georgy.keranov@gmail.com>
2021-09-04 17:53:42 +0300

    Change svg function prefix

--- inc/load-svg-icon.php
+++ inc/load-svg-icon.php
@@ -1,6 +1,6 @@
 <?php
 
-function idt_get_svg( $svg_name ) {
+function gk_get_svg( $svg_name ) {
 	$svg_url = get_template_directory_uri() . '/images/' . $svg_name . '.svg';
 
 	$svg = file_get_contents( $svg_url );
