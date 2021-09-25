6301a83b093ed39bae07e10202fb1d911e8135a6
Georgi <georgy.keranov@gmail.com>
2021-09-25 18:10:12 +0300

    Change html and styles for section projects

--- template-parts/section-projects.php
+++ template-parts/section-projects.php
@@ -22,45 +22,40 @@ if ( empty( $args['projects'] ) ) {
 			$logo_url = wp_get_attachment_image_url( $logo_id, 'medium' );
 		}
 
-		$technologies = carbon_get_post_meta( $id, 'gk_technologies' );
+		$main_technologies = carbon_get_post_meta( $id, 'gk_main_technologies' );
 		$excerpt = get_the_excerpt( $id );
 		?>
 		
 		<div class="section__project">
-			<div class="section__project-inner">
-				<div class="section__image-background<?php echo !empty( $logo_url ) ? ' section__image-background--with-logo' : '' ?>" style="background-image:url(<?php echo $image_url ?>)">
+			<div class="section__project-background" style="background-image:url(<?php echo $image_url ?>)">
+				<div class="section__project-content">
 					<?php if ( !empty( $logo_url ) ) : ?>
 						<div class="section__image-logo" style="background-image:url(<?php echo $logo_url ?>)"></div>
+					<?php else : ?>
+						<div class="section__title">
+							<h2><?php echo esc_html( $title ) ?></h2>
+						</div><!-- /.section__title -->
 					<?php endif; ?>
+
+					<div class="section__details">
+						<div class="section__excerpt">
+							<?php echo wpautop( $excerpt ) ?>
+						</div><!-- /.section__excerpt -->
+
+						<?php if ( !empty( $main_technologies ) ) : ?>
+							<ul class="section__technologies">
+								<?php foreach ( $main_technologies as $technology ) : ?>
+									<li>
+										<p><?php echo esc_html( $technology['technology'] ) ?></p>
+									</li>
+								<?php endforeach; ?>
+							</ul>
+						<?php endif; ?>
+					</div><!-- /.section__details -->
 
 					<a href="<?php echo $permalink ?>"></a>
-				</div>
-
-				<div class="section__details">
-					<?php if ( !empty( $title ) ) : ?>
-						<h4><a href="<?php echo $permalink ?>"><?php echo esc_html( $title ) ?></a></h4>
-					<?php endif; ?>
-
-
-					<div class="section__excerpt">
-						<?php echo wpautop( $excerpt ) ?>
-					</div><!-- /.section__excerpt -->
-
-					<?php if ( !empty( $technologies ) ) : ?>
-						<ul class="section__technologies">
-							<?php foreach ( $technologies as $technology ) : ?>
-								<li>
-									<p><?php echo esc_html( $technology['technology'] ) ?></p>
-								</li>
-							<?php endforeach; ?>
-						</ul>
-					<?php endif; ?>
-				</div><!-- /.section__details -->
-
-				<div class="section__action">
-					<?php gk_render_button( __( 'View Project', 'gk' ), $permalink, 'no', 'btn' ) ?>
-				</div><!-- /.section__action -->
-			</div><!-- /.section__project-inner -->
+				</div><!-- /.section__project-content -->
+			</div><!-- /.section__project-background -->
 		</div><!-- /.section__project -->
 	<?php endforeach; ?>
 </div><!-- /.section-projects -->
