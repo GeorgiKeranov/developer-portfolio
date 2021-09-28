1a48740ee156bc7c548ba3a2615d4add3bb0be7a
Georgi <georgy.keranov@gmail.com>
2021-09-28 16:11:47 +0300

    Add fix for partciles js when there is no section intro

--- js/particles/particles.js
+++ js/particles/particles.js
@@ -1,5 +1,11 @@
 import 'particles.js'
 
 export default function particles() {
+	let $sectionIntro = $('#section-intro-0');
+
+	if (!$sectionIntro.length) {
+		return;
+	}
+
 	particlesJS.load('section-intro-0', globalVariables.siteUrl + '/js/particles/particles.json');
 }
