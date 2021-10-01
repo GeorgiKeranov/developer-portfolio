f508c9b1bab651f5fb2d1cffc3931ca1f08930e9
Georgi <georgy.keranov@gmail.com>
2021-10-01 19:47:40 +0300

    Add html and integration for the section words with gallery

---
+++ template-parts/page-builder/section-words-with-gallery.php
@@ -0,0 +1,38 @@
+<div class="section-words-with-gallery">
+	<div class="container">
+		<?php if ( !empty( $args['title'] ) ) : ?>
+			<h2><?php echo esc_html( $args['title'] ) ?></h2>
+		<?php endif; ?>
+
+		<?php if ( !empty( $args['description'] ) ) : ?>
+			<h3><?php echo esc_html( $args['description'] ) ?></h3>
+		<?php endif; ?>
+
+		<?php if ( !empty( $args['words'] ) ) : ?>
+			<ul class="section__words">
+				<?php foreach ( $args['words'] as $word ) : ?>
+					<li>
+						<p><?php echo esc_html( $word['word'] ) ?></p>
+					</li>
+				<?php endforeach; ?>
+			</ul><!-- /.section__words -->
+		<?php endif; ?>
+
+		<?php if ( !empty( $args['gallery'] ) ) : ?>
+			<div class="section__gallery">
+				<?php foreach ( $args['gallery'] as $image ) :
+					$image_url = wp_get_attachment_image_url( $image, 'large' );
+
+					if ( empty( $image_url ) ) {
+						continue;
+					}
+					?>
+
+					<div class="section__image">
+						<div class="section__background-image" style="background-image: url(<?php echo $image_url ?>)"></div><!-- /.section__background-image -->
+					</div><!-- /.section__image -->
+				<?php endforeach; ?>
+			</div><!-- /.section__gallery -->
+		<?php endif; ?>
+	</div><!-- /.container -->
+</div><!-- /.section-words-with-gallery -->
