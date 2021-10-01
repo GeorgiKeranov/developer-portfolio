ed6c6da750419fdfc1501e44bb4ada03aea641b7
Georgi <georgy.keranov@gmail.com>
2021-10-01 14:20:50 +0300

    Add integration for the new background image field

--- template-parts/page-builder/section-text.php
+++ template-parts/page-builder/section-text.php
@@ -1,4 +1,14 @@
-<div class="section-text">
+<?php 
+$style = '';
+$class = '';
+
+if ( !empty( $args['background_image'] ) ) {
+	$style = ' style="background-image:url(' . wp_get_attachment_image_url( $args['background_image'], 'full' ) . ')"';
+	$class = ' section-text--image-background';
+}
+?>
+
+<div class="section-text<?php echo $class ?>"<?php echo $style ?>>
 	<div class="container">
 		<?php echo apply_filters( 'the_content', $args['text'] ) ?>		
 	</div><!-- /.container -->
