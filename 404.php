b692915a9fb899e1a85a4c31681428eda30c339f
Georgi <georgy.keranov@gmail.com>
2021-10-05 20:13:56 +0300

    Add html and styles for section page and 404

--- 404.php
+++ 404.php
@@ -10,51 +10,19 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="section-404">
+	<div class="container">
+		<div class="section__inner">
+			<h1>404</h1>
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'gk' ); ?></h1>
-			</header><!-- .page-header -->
+			<h2><?php _e( 'Page not found', 'idt' ) ?></h2>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'gk' ); ?></p>
+			<p><?php _e( 'You can try to open the home page with the button below.', 'idt' ) ?>.</p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'gk' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$gk_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'gk' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$gk_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+			<a href="<?php echo home_url('/') ?>" class="btn"><?php _e( 'Home Page' ) ?></a>
+		</div><!-- /.section__inner -->
+	</div><!-- /.container -->
+</div><!-- /.section-404 -->
 
 <?php
 get_footer();
--- page.php
+++ page.php
@@ -15,24 +15,17 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div class="section-page">
+	<div class="container">
+		<div class="section__title">
+			<h1><?php the_title() ?></h1>
+		</div><!-- /.section__title -->
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+		<div class="section__content">
+			<?php the_content() ?>
+		</div><!-- /.section__content -->
+	</div><!-- /.container -->
+</div><!-- /.section-page -->
 
 <?php
-get_sidebar();
 get_footer();
